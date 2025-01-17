be08253b70e16e18a556d21f5fac419f3fd541fe
ILLBADLY <u.artemiy@bk.ru>
2025-01-17 14:12:54 +0800

    3

--- rey/Source.cpp
+++ rey/Source.cpp
@@ -14,6 +14,14 @@ int main()
 	float speed = 2;
 	Vector2 mPos{ 0,0 };
 	Vector2 playerCenter;
+	float rot;
+
+
+	Texture2D playerTexture = LoadTexture("arc.png");
+
+
+
+
 	while (!WindowShouldClose())
 	{
 		if (IsKeyDown(KEY_A))
@@ -44,16 +52,32 @@ int main()
 		dir.y = 0;
 		mPos = GetMousePosition();
 		playerCenter = Vector2{ playerRect.x + playerRect.width / 2, playerRect.y + playerRect.height / 2 };
+		rot = acos(Vector2Subtract(playerCenter, mPos).x / Vector2Length(Vector2Subtract(playerCenter, mPos)));
+		if (Vector2Subtract(playerCenter, mPos).y < 0)
+		{
+			rot = -rot;
+		}
 
-
+		BeginDrawing();
+		ClearBackground(LIGHTGRAY);
 		DrawFPS(0,0);
-		ClearBackground(VIOLET);
+		/*ClearBackground(VIOLET);*/
 
 		DrawFPS(0, 0);
 		Vector2 mPos = GetMousePosition();
-		DrawRectangleRec(playerRect, RED);
+		DrawTexturePro(
+						playerTexture,
+						{ 0,0, (float)playerTexture.width, (float) playerTexture.height },
+						playerRect,
+			{ playerRect.width / 2, playerRect.height / 2},
+			rot * RAD2DEG, WHITE
+			);
+		/*DrawRectangleRec(playerRect, RED);*/
+
+
+
 	/*	DrawLine(mPos.x, mPos.y, playerRect.width / 2, x+playerRect.y, playerRect.height / 2, BLUE);*/
-		DrawLineEx(mPos, Vector2{ playerRect.x + playerRect.width / 2, playerRect.y + playerRect.height / 2 }, 5, BLUE);
+		/*DrawLineEx(mPos, Vector2{ playerRect.x + playerRect.width / 2, playerRect.y + playerRect.height / 2 }, 5, BLUE)*/;
 		/*rawRectangle(mPos.x,mPos.y, 300, 50, GREEN);*/
 		/*IsMouseButtonDown(MOUSE_BUTTON_LEFT);
 		IsKeyDown(KEY_A);*/
