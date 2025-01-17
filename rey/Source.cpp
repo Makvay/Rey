22713f64e845194083aa4897a9da3e1f578e9f07
ILLBADLY <u.artemiy@bk.ru>
2025-01-17 13:35:31 +0800

    2

--- rey/Source.cpp
+++ rey/Source.cpp
@@ -8,10 +8,12 @@ int main()
 	int screenWidth = 800;
 	int screenHeigh = 600;
 	InitWindow(screenWidth, screenHeigh, "Test");
-	SetTargetFPS(240);
+	SetTargetFPS(240 );
 	Rectangle playerRect{screenWidth/2, screenHeigh/2, 50,50};
 	Vector2 dir{ 0,0 };
 	float speed = 2;
+	Vector2 mPos{ 0,0 };
+	Vector2 playerCenter;
 	while (!WindowShouldClose())
 	{
 		if (IsKeyDown(KEY_A))
@@ -40,14 +42,18 @@ int main()
 	
 		dir.x = 0;
 		dir.y = 0;
+		mPos = GetMousePosition();
+		playerCenter = Vector2{ playerRect.x + playerRect.width / 2, playerRect.y + playerRect.height / 2 };
 
 
-		BeginDrawing();
+		DrawFPS(0,0);
 		ClearBackground(VIOLET);
 
 		DrawFPS(0, 0);
 		Vector2 mPos = GetMousePosition();
 		DrawRectangleRec(playerRect, RED);
+	/*	DrawLine(mPos.x, mPos.y, playerRect.width / 2, x+playerRect.y, playerRect.height / 2, BLUE);*/
+		DrawLineEx(mPos, Vector2{ playerRect.x + playerRect.width / 2, playerRect.y + playerRect.height / 2 }, 5, BLUE);
 		/*rawRectangle(mPos.x,mPos.y, 300, 50, GREEN);*/
 		/*IsMouseButtonDown(MOUSE_BUTTON_LEFT);
 		IsKeyDown(KEY_A);*/
